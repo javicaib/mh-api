894177ad2d725f0d0889ba3430654d36dbce694e
Javier Antonio Roque Sañudo <javierars@uci.cu>
2024-03-03 22:36:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { findAllMonsters, findByMonsters } from "./mongo.js";
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+
+app.get('/all', (req, res) => {
+    const { page } = req.query
+    findAllMonsters(page).then((data) =>
+        res.json(data).status(200)
+    )
+    console.log(req.query)
+});
+
+app.get('/search', (req, res) => {
+    const { name, weak, type } = req.query
+    if (name) {
+        return findByMonsters("name", name).then(data => {
+            res.json(data).status(200)
+        })
+    }
+    if (weak) {
+        return findByMonsters("weak", weak).then(data => {
+            res.json(data).status(200)
+        })
+    }
+    if (type) {
+
+        return findByMonsters("type", type).then(data => {
+            res.json(data).status(200)
+        })
+    }
+    else {
+        return res.status(400).json({ "error": "parámetro no permitido" })
+    }
+}
+
+);
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
---
+++ mongo.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+dotenv.config()
+console.log(dotenv);
+
+
+try {
+    mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB CONNECT"))
+} catch (error) {
+    console.log(error)
+}
+
+const monsterSchema = mongoose.Schema({
+    name: String,
+    type: String,
+    weakness: [String],
+})
+monsterSchema.plugin(mongoosePaginate)
+const Modelo = mongoose.model('monsters', monsterSchema);
+
+
+export async function findAllMonsters(page) {
+    const options = {
+        page: Number(page),
+        limit: 10,
+    };
+    return await Modelo.paginate({}, options).then({})
+}
+
+export async function findByMonsters(query, param) {
+    if (query === "name") {
+        return await Modelo.find({ name: { "$regex": param } })
+    }
+    if (query === "type") {
+        return await Modelo.find({ type: { "$regex": param } })
+    }
+    if (query === "weak") {
+        return await Modelo.find({ weakness: { "$regex": param } })
+    }
+    else return false
+}
